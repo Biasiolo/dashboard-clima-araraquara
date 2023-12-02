dbbd1ab14f86758d37bd3453b278f30c230cd77a
Biasiolo <biasiolorafael@gmail.com>
2023-12-02 16:32:37 -0300

    ajustes css

--- plugin-screen-color.js
+++ plugin-screen-color.js
@@ -3,47 +3,38 @@
         var settings = $.extend({
             colors: [
                 { name: "blue", color: "rgb(43, 174, 226)" },
-                { name: "green", color: "rgb(43, 226, 113)" },
                 { name: "yellow", color: "rgb(177, 226, 43)" },
-                { name: "purple", color: "rgb(159, 43, 226)" },
+                { name: "white", color: "rgb(255, 255, 225)" },
                 { name: "red", color: "rgb(214, 40, 40)" },
                 { name: "black", color: "#080808" },
             ],
-            initialColor: "yellow", // Adiciona uma nova opção para a cor inicial
 
-            textColors: [ // Add an array of contrasting text colors
-                { name: "blueText", color: "#66250B" }, // Contrast for blue
-                { name: "greenText", color: "#A60D2C" }, // Contrast for green
-                { name: "yellowText", color: "#080808" }, // Contrast for yellow
-                { name: "purpleText", color: "#020827" }, // Contrast for purple
-                { name: "redText", color: "#D9FEBE" }, // Contrast for black
-                { name: "blackText", color: "#fff" }, // Contrast for black
-            ]
+            initialColor: "white", // Adiciona uma nova opção para a cor inicial
+
+            textColors: [ // Adicione um array de cores de texto de contraste predefinidas
+                { name: "blue", color: "#66250B" },
+                { name: "yellow", color: "#080808" },
+                { name: "white", color: "#020827" },
+                { name: "red", color: "#D9FEBE" },
+                { name: "black", color: "#fff" },
+            ],
         }, options);
 
-        // Encontrar o índice da cor inicial
-        var initialColorIndex = settings.colors.findIndex(color => color.name === settings.initialColor);
-
-        // Inicia com a cor inicial ou, se não for encontrada, com a primeira cor
-        var currentColorIndex = (initialColorIndex !== -1) ? initialColorIndex : 0;
+        var currentColorIndex = settings.colors.findIndex(color => color.name === settings.initialColor) || 0;
 
         function changeColor() {
             currentColorIndex = (currentColorIndex + 1) % settings.colors.length;
             updateColor();
         }
 
-
-
         function updateColor() {
             var selectedColor = settings.colors[currentColorIndex].color;
             var selectedTextColor = settings.textColors[currentColorIndex].color;
+
             $("body").css({
                 "background-color": selectedColor,
-                "color": selectedTextColor // Set the contrasting text color
+                "color": selectedTextColor,
             });
-
-            var textColor = getContrastYIQ(selectedColor);
-            $("body").css("color", textColor); // Set text color to ensure contrast
 
             // Adiciona a classe 'rotacionar' ao display
             $(".scroll-container").addClass("rotacionar");
